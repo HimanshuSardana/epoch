c3c246f8d72b9b18d7a0a4a47c82600b9c95fdf0
Himanshu Sardana <himanshusardana2005@gmail.com>
2026-03-15 01:52:42 +0530

    feat: init git dir structure

--- main.go
+++ main.go
@@ -4,13 +4,23 @@ import (
 	"fmt"
 	"io/fs"
 	"log"
+	"os"
 	"path/filepath"
 )
 
 func main() {
-	root := "./"
+	root := "./test/"
 
-	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+	err := os.Mkdir(root+".git/", 0o755)
+	err = os.Mkdir(root+".git/objects/", 0o755)
+	err = os.Mkdir(root+".git/refs/", 0o755)
+	err = os.WriteFile(root+".git/HEAD", nil, 0o755)
+	err = os.WriteFile(root+".git/config", nil, 0o755)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
